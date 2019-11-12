18074d6484d9556ad6e456c2a801510de57b5050
Stefan Gherman <scgheman@gmail.com>
2019-11-12 04:48:10 +0200

    Fix an age checking issue.
    Comparison was done between string and int, changed to to string and string.

--- common.py
+++ common.py
@@ -59,7 +59,7 @@ print(generate_random([['kH14Ju#&', '1', '21', '2016', 'in', '31']]))
 
 def check_age(string):
     digits = ['0','1','2','3','4','5','6','7','8','9']
-    if string[FIRST_CHAR] == 0:
+    if string[FIRST_CHAR] == '0':
         return False
     for char in string:
         if char not in digits:
